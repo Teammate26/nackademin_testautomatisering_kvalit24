67cf7a2e2ba8bc5546d74c625a9d86d0b377d5a2
Jesper Sjöberg <jesper26sjoberg@live.se>
2025-09-25 10:08:27 +0200

    Conftest update

--- labs/06_07_08_test_framework/test/conftest.py
+++ labs/06_07_08_test_framework/test/conftest.py
@@ -6,8 +6,7 @@ BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
 
 
 def pytest_sessionstart(session):
-    api_base = "BACKEND_URL, http://localhost:8000"
-    user_api = UserAPI(api_base)
+    user_api = UserAPI(BACKEND_URL)
 
     # Create users
     user_api.signup("admin", "1234")
@@ -16,7 +15,7 @@ def pytest_sessionstart(session):
 
     # Login as admin to create products
     user_api.login("admin", "1234")
-    admin_api = AdminAPI(api_base, token=user_api.token)
+    admin_api = AdminAPI(BACKEND_URL, token=user_api.token)
     admin_api.create_product("testproduct00")
     admin_api.create_product("testproduct11")
 
@@ -25,5 +24,4 @@ def pytest_sessionstart(session):
     user_api.add_product_to_user("testproduct00")
     user_api.add_product_to_user("testproduct11")
 
-    # testuser11 has no products
     print("Test data setup via API completed!")
